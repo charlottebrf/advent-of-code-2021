ecd0e53bf94623bb170365666d99df055f6afc0d
Charlotte Fereday <cbrfereday@gmail.com>
2021-12-05 19:32:21 +0000

    feat: adds solution for day 1

---
+++ day1/day1.ts
@@ -0,0 +1,40 @@
+const fs = require('fs')
+
+const isBigger = (beforeCurrent, current) => {
+    return current > beforeCurrent
+} 
+
+const reportOutcome = {
+    none: '(N/A - no previous measurement)',
+    increased: '(increased)',
+    decreased: '(decreased)'
+}
+
+const createOutput = (item, outcome) => {
+    return `${item} ${outcome}`
+}
+
+try {
+    const data = fs.readFileSync( "input.txt", 'utf8')
+    const report = data.split('\n')
+    const finalStrings = [];
+
+    for (const stringifiedNum in report) {
+        const itemIndex = parseInt(stringifiedNum);
+        const itemBeforeCurrentIndex = itemIndex - 1;
+        let finalString
+
+        if (itemBeforeCurrentIndex < 0) {
+            finalString = createOutput(report[stringifiedNum], reportOutcome.none)
+            finalStrings.push(finalString)
+        } else {
+            const isItBigger = isBigger(report[itemBeforeCurrentIndex], parseInt(report[stringifiedNum]))
+            isItBigger ? finalString = createOutput(report[stringifiedNum], reportOutcome.increased) : finalString = createOutput(report[stringifiedNum], reportOutcome.decreased)
+            finalStrings.push(finalString)
+        }
+    }
+    console.log(finalStrings)
+    }
+   catch (err) {
+    console.error(err)
+  }
